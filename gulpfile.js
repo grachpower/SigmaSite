b5ff3616d3c12c037c1414de89cee5952508505c
Duke <amiscary@gmail.com>
2016-09-13 16:48:56 +0300

    modified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,7 @@ var gulp = require('gulp'), // Подключаем Gulp
     imagemin = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
     pngquant = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
     htmlmin = require('gulp-htmlmin'), // Подключаем библиотеку для минификации html
-    imageminJpegtran = require('imagemin-jpegtran'), // Подключаем библиотеку для минификации JPEG
-    tinypng = require('gulp-tinypng'); // подключаем библиотеку для минификации изображений
+    imageminJpegtran = require('imagemin-jpegtran'); // Подключаем библиотеку для минификации JPEG
 
 // собираем css - gulp css-libs
 gulp.task('css-libs', function() {
@@ -20,27 +19,14 @@ gulp.task('htmlmin', function() {
         .pipe(htmlmin({
             collapseWhitespace: true
         }))
-        .pipe(gulp.dest('dist/minHtml'))
-})
+        .pipe(gulp.dest('dist/'));
+});
 
 //сжимаем изображения
 gulp.task('img', function() {
-    return gulp.src('src/img/**/*') // Берем все изображения из src
-        .pipe(imagemin({ // Сжимаем их с наилучшими настройками
-            interlaced: true,
-            progressive: true,
-            svgoPlugins: [{
-                removeViewBox: false
-            }],
-            use: [pngquant(), imageminJpegtran()]
-        }))
-        .pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
-});
-
-gulp.task('tinypng', ['img'], function() {
     return gulp.src('src/img/**/*')
-        .pipe(tinypng('API_KEY'))
-        .pipe(gulp.dest('dist/img'));
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img/'));
 });
 
 // очищаем папку dist преред сборкой - gulp clean
@@ -49,4 +35,4 @@ gulp.task('clean', function() {
 });
 
 // собираем проект
-gulp.task('build', ['clean', 'img', 'htmlmin', 'css-libs', 'scripts'], function() {});
+gulp.task('build', ['clean', 'img', 'htmlmin', 'css-libs'], function() {});
