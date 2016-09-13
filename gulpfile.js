5086c8b6f2ecd3a45d053f3f93fd805eb20d135a
Duke <amiscary@gmail.com>
2016-09-13 14:41:53 +0300

    modified pagination

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 // подключение пакетов
 var gulp = require('gulp'),
  cssmin = require('gulp-cssmin'),
+ htmlmin = require('gulp-htmlmin'),
  spritesmith = require('gulp.spritesmith');
 
 // объявление задачи
-gulp.task('sprite', function () {
-  return gulp.src('icons/*.png')
-  	.pipe(spritesmith({
-	    imgName: 'sprite.png',
-	    cssName: 'sprite.css'
-  	}))
-  	.pipe(cssmin())
-  	.pipe(gulp.dest('dist/'));
+gulp.task('minifyhtml', function() {
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('default', function () {
+    gulp.src('src/**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
 });
