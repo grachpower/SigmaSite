02e98d65fc1dc8e9a27bafb3adb203fbafe1a15a
Duke <amiscary@gmail.com>
2016-09-19 11:27:29 +0300

    added scss styles

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,22 @@ var gulp = require('gulp'), // Подключаем Gulp
     del = require('del'), // Подключаем библиотеку для удаления файлов и папок
     imagemin = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
     pngquant = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
-    htmlmin = require('gulp-htmlmin'), // Подключаем библиотеку для минификации html
-    imageminJpegtran = require('imagemin-jpegtran'); // Подключаем библиотеку для минификации JPEG
+    htmlmin = require('gulp-html-minifier'), // Подключаем библиотеку для минификации html
+    imageminJpegtran = require('imagemin-jpegtran'), // Подключаем библиотеку для минификации JPEG
+    watch = require('gulp-watch'),
+    sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('.src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('.src/css'));
+});
 
 // собираем css - gulp css-libs
 gulp.task('css-libs', function() {
-    return gulp.src('src/css/style.css') // Выбираем файл для минификации
-        .pipe(cssnano()) // Сжимаем
-        .pipe(gulp.dest('dist/css')); // Выгружаем в папку dist/css
+    return gulp.src('src/css/style.css')
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/css'));
 });
 
 // минифицируем html
@@ -31,7 +39,7 @@ gulp.task('img', function() {
 
 // очищаем папку dist преред сборкой - gulp clean
 gulp.task('clean', function() {
-    return del.sync('dist'); // Удаляем папку dist перед сборкой
+    return del.sync('dist');
 });
 
 // собираем проект
